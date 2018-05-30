e3c6c0e4494517223628b71d6ab4e8ce96ec2531
Spiros Dontas <spirosdontas@gmail.com>
2018-05-30 19:26:19 +0300

    ioctl

--- virtio-crypto/guest/crypto-chrdev.c
+++ virtio-crypto/guest/crypto-chrdev.c
@@ -214,14 +214,14 @@ static long crypto_chrdev_ioctl(struct file *filp, unsigned int cmd,
                        return_sg,
                        *sgs[8];
 	unsigned int num_out, num_in, len;
-    unsigned int sess_id;
+    unsigned int *sess_id;
 	unsigned char *key,
                   *src,
                   *iv,
                   *dst;
 	unsigned int *syscall_type;
-    struct session_op sess;
-    struct crypt_op cryp;
+    struct session_op *sess;
+    struct crypt_op *cryp;
 
 	debug("Entering");
 
@@ -261,15 +261,15 @@ static long crypto_chrdev_ioctl(struct file *filp, unsigned int cmd,
 		/* sgs[num_out++] = &output_msg_sg; */
 		/* sg_init_one(&input_msg_sg, input_msg, MSG_LEN); */
 		/* sgs[num_out + num_in++] = &input_msg_sg; */
-        &sess = (struct session_op *) arg;
+        sess = (struct session_op *) arg;
         /* if (copy_from_user(&sess, (struct session_op *) arg, sizeof(struct session_op))) { */
             /* debug("copy from user fail"); */
             /* return -EFAULT; */
         /* } */
-        key = (unsigned char *) sess.key;
+        key = (unsigned char *) sess->key;
         debug("key pointer is %p", (void *) key);
-        debug("size of key is %d", sess.keylen);
-        sg_init_one(&session_key_sg, key, sess.keylen);
+        debug("size of key is %d", sess->keylen);
+        sg_init_one(&session_key_sg, key, sess->keylen);
         sgs[num_out++] = &session_key_sg;
         sg_init_one(&session_op_sg, &sess, sizeof(sess));
         sgs[num_out + num_in++] = &session_op_sg;
@@ -285,13 +285,13 @@ static long crypto_chrdev_ioctl(struct file *filp, unsigned int cmd,
 		/* sgs[num_out++] = &output_msg_sg; */
 		/* sg_init_one(&input_msg_sg, input_msg, MSG_LEN); */
 		/* sgs[num_out + num_in++] = &input_msg_sg; */
-        &sess_id = (unsigned int *) arg;
+        sess_id = (unsigned int *) arg;
         /* if (copy_from_user(&sess_id, (unsigned int *) arg, sizeof(unsigned int))) { */
             /* debug("copy from user fail"); */
             /* return -EFAULT; */
         /* } */
         debug("sess id %d", sess_id);
-        sg_init_one(&session_id_sg, &sess_id, sizeof(sess_id));
+        sg_init_one(&session_id_sg, sess_id, sizeof(*sess_id));
         sgs[num_out++] = &session_id_sg;
         sg_init_one(&return_sg, &host_ret, sizeof(host_ret));
         sgs[num_out + num_in++] = &return_sg;
@@ -305,22 +305,22 @@ static long crypto_chrdev_ioctl(struct file *filp, unsigned int cmd,
 		/* sgs[num_out++] = &output_msg_sg; */
 		/* sg_init_one(&input_msg_sg, input_msg, MSG_LEN); */
 		/* sgs[num_out + num_in++] = &input_msg_sg; */
-        &cryp = (struct crypt_op *) arg;
+        cryp = (struct crypt_op *) arg;
         /* if (copy_from_user(&cryp, (struct crypt_op *) arg, sizeof(struct crypt_op))) { */
             /* debug("copy from user fail"); */
             /* return -EFAULT; */
         /* } */
-        src  = (unsigned char *) cryp.src;
-        dst  = (unsigned char *) cryp.dst;
-        iv   = (unsigned char *) cryp.iv;
-        debug("data len is %d", cryp.len);
+        src  = (unsigned char *) cryp->src;
+        dst  = (unsigned char *) cryp->dst;
+        iv   = (unsigned char *) cryp->iv;
+        debug("data len is %d", cryp->len);
         sg_init_one(&crypt_op_sg, &cryp, sizeof(cryp));
         sgs[num_out++] = &crypt_op_sg;
-        sg_init_one(&src_sg, src, cryp.len);
+        sg_init_one(&src_sg, src, cryp->len);
         sgs[num_out++] = &src_sg;
         sg_init_one(&iv_sg, iv, AES_BLOCK_LEN);
         sgs[num_out++] = &iv_sg;
-        sg_init_one(&dst_sg, dst, cryp.len);
+        sg_init_one(&dst_sg, dst, cryp->len);
         sgs[num_out + num_in++] = &dst_sg;
         sg_init_one(&return_sg, &host_ret, sizeof(host_ret));
         sgs[num_out + num_in++] = &return_sg;

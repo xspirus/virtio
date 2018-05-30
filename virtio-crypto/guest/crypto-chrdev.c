68abe0173465edaf81053b607873f1ac913bd2cb
Spiros Dontas <spirosdontas@gmail.com>
2018-05-30 19:27:42 +0300

    ioctl

--- virtio-crypto/guest/crypto-chrdev.c
+++ virtio-crypto/guest/crypto-chrdev.c
@@ -290,7 +290,7 @@ static long crypto_chrdev_ioctl(struct file *filp, unsigned int cmd,
             /* debug("copy from user fail"); */
             /* return -EFAULT; */
         /* } */
-        debug("sess id %d", sess_id);
+        debug("sess id %d", *sess_id);
         sg_init_one(&session_id_sg, sess_id, sizeof(*sess_id));
         sgs[num_out++] = &session_id_sg;
         sg_init_one(&return_sg, &host_ret, sizeof(host_ret));
